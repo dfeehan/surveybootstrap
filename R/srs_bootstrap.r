d42ef57cee48690fbaa3c4b462cd088e0ed43acb
dennis feehan <feehan@berkeley.edu>
2022-12-13 22:30:54 -0800

    markdownifying

--- R/srs_bootstrap.r
+++ R/srs_bootstrap.r
@@ -2,24 +2,23 @@
 #####################################################
 ##' srs.bootstrap.sample
 ##'
-##' given a survey dataset and a description of the survey
+##' Given a survey dataset and a description of the survey
 ##' design (ie, which combination of vars determines primary sampling
 ##' units, and which combination of vars determines strata), take
 ##' a bunch of bootstrap samples under a simple random sampling
 ##' (with repetition) scheme
 ##'
-##' @param survey.data the dataset to use
-##' @param num.reps the number of bootstrap replication samples to draw
-##' @param parallel if TRUE, use parallelization (via `plyr`)
-##' @param paropts an optional list of arguments passed on to `plyr` to control
-##'        details of parallelization
-##' @param ... ignored, but useful because it allows params like
-##\code{survey.design},
-##' which are used in other bootstrap designs, to be passed in without error
-##' @return a list with `num.reps` entries. each entry is a dataset which has
-##' at least the variables `index` (the row index of the original dataset that
-##' was resampled) and `weight.scale` (the factor by which to multiply the
-##' sampling weights in the original dataset).
+##' @param survey.data The dataset to use
+##' @param num.reps The number of bootstrap replication samples to draw
+##' @param parallel If `TRUE`, use parallelization (via `plyr`)
+##' @param paropts An optional list of arguments passed on to `plyr` to control
+##'   details of parallelization
+##' @param ... Ignored, but useful because it allows params like `survey.design`
+##'   which are used in other bootstrap designs, to be passed in without error
+##' @return A list with `num.reps` entries. Each entry is a dataset which has
+##'   at least the variables `index` (the row index of the original dataset that
+##'   was resampled) and `weight.scale` (the factor by which to multiply the
+##'   sampling weights in the original dataset).
 ##'
 ##' @export
 srs.bootstrap.sample <- function(survey.data,
